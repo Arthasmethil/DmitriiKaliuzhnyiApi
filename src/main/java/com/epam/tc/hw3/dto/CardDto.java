e73d716feff6c8a3fe23d11b8285db8ad4cc636d
Dmitrii Kaliuzhnyi <dmitrii_kaliuzhnyi@epam.com>
2022-03-28 21:12:56 +0300

    -Create DTO classes -Separate Constants and Endpoints

---
+++ src/main/java/com/epam/tc/hw3/dto/CardDto.java
@@ -0,0 +1,11 @@
+package com.epam.tc.hw3.dto;
+
+import lombok.Data;
+
+@Data
+public class CardDto {
+
+    private String id;
+    private String idList;
+    private String name;
+}
---
+++ src/main/java/com/epam/tc/hw3/dto/LabelDto.java
@@ -0,0 +1,12 @@
+package com.epam.tc.hw3.dto;
+
+import lombok.Data;
+
+@Data
+public class LabelDto {
+
+    private String id;
+    private String idCard;
+    private String name;
+    private String color;
+}
